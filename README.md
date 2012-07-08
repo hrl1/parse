a085760334144be5867c726446c98d14fee411ba
hrl1 <hrl1@sfu.ca>
2012-07-08 03:39:43 -0700

    first commit of parsing

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-parse
+parsing
 =====
+
+First change config.json to whatever options you prefer, then and 
+
+	npm install
+
+packages. Then run 
+
+	node dict_to_db.js
+
+to fill the database. (client currently doesn't close the connection so do it manually after a few seconds. sorry).
+To test sentences, you can change the "query" string in test.js, then run 
+
+	node test.js
+
---
+++ dict_to_db.js
@@ -0,0 +1,31 @@
+var	fs = require('fs'),
+	config = JSON.parse(fs.readFileSync('config.json')),
+	text = fs.readFileSync('brown_nostem.txt').toString().split('\n'),
+	client = require('redis').createClient(config.database.port, config.database.host);
+
+var data = [];
+var count = text.length;
+for(i in text) {
+	data.push(text[i]);
+}
+
+client.select(config.database["db-num"], function(err, result) {
+	if(err) {
+		console.log(err);
+		client.end();
+	}
+})
+
+for(i in data) {
+	var line = data[i].split(' ');
+	console.log(line);
+	client.zadd('dict', parseFloat(line[1]), line[0], function(err, result) {
+		if (err) {
+			console.log(err);
+		}
+	})
+	if (--count === 0) {
+		console.log('count: ' + text.length);
+		//client.end();
+	}
+}
